91ad28e264b5ae5ec0a9b057a8499fb376618b54
lucas.yan <lucas.yan@automind.com.br>
2021-11-03 09:07:28 -0300

    Adding users password to database

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,5 +10,9 @@ namespace API.Entities
         public string Id { get; set; }
 
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        
+        public byte[] PasswordSalt { get; set; }
     }
 }
